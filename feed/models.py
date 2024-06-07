d6291f2224bc9341856ba6ab53d60c2e23498538
laud <laudbentil@gmail.com>
2024-06-07 00:00:12 +0000

    create empty models

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Company(models.Model):
+    pass
+
+
+class Location(models.Model):
+    pass
+
+
+class Website(models.Model):
+    pass
+
+
+class Feed(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass)
