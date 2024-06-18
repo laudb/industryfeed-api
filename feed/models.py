12effa9942003ec6baeba0a29408644e7dd4fa34
laud <laudbentil@gmail.com>
2024-06-18 03:59:46 +0000

    define initial models

--- feed/models.py
+++ feed/models.py
@@ -3,11 +3,21 @@ from django.db import models
 
 # Create your models here.
 class Company(models.Model):
-    pass
+    signature = models.UUIDField()
+    logo = models.ImageField()
+    name = models.CharField()
+    details = models.TextField()
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated_at = models.DateTimeField(auto_now=True)
 
 
 class Location(models.Model):
-    pass
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    town = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
 
 
 class Website(models.Model):
@@ -19,8 +29,10 @@ class Website(models.Model):
 
 
 class Feed(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    code = models.UUIDField()
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
