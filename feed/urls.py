8eecdd79f045461918142ac06c7eadf72fde7df4
laud <laudbentil@gmail.com>
2024-06-19 13:54:43 +0000

    use default router to setup urls

--- feed/urls.py
+++ feed/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from feed import views
 
+
+router = DefaultRouter()
+router.register(r"companies", views.CompanyViewSet, basename="company")
+router.register(r"feeds", views.FeedViewSet, basename="feed")
+router.register(r"websites", views.WebsiteViewSet, basename="website")
+router.register(r"locations", views.LocationViewSet, basename="location")
+
 urlpatterns = [
-    path("company/", views.CompanyViewSet),
-    path("feed/", views.FeedViewSet),
-    path("website/", views.WebsiteViewSet),
-    path("location/", views.LocationViewSet),
+    path("", include(router.urls)),
 ]
