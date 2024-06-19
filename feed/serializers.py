6d60d111d5eca863ab9fe4046d5f2d6a8c46a5d8
laud <laudbentil@gmail.com>
2024-06-19 13:09:27 +0000

    add Company, Feed, Website, Location serializers

---
+++ feed/serializers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from .models import Company, Feed, Website, Location
+from rest_framework import serializers
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ["name", "details", "logo"]
+
+
+class FeedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feed
+        fields = ["name", "code"]
+
+
+class WebsiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Website
+        fields = ["name", "type", "url"]
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ["lat", "long", "town", "state", "country"]
