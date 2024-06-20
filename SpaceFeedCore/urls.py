257de7443667f3fadb63ee02721bbb3601cfc28e
laud <laudbentil@gmail.com>
2024-06-20 03:00:51 +0000

    fix typo

--- SpaceFeedCore/urls.py
+++ SpaceFeedCore/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("feed.urls")),
-    path("api/v1/auth", include("djsoser.urls")),
-    path("api/v1/auth", include("djsoser.urls.authtoken")),
-    path("api/v1/auth", include("djsoser.urls.jwt")),
+    path("api/v1/auth", include("djoser.urls")),
+    path("api/v1/auth", include("djoser.urls.authtoken")),
+    path("api/v1/auth", include("djoser.urls.jwt")),
 ]
