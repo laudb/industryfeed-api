67fb55d724700244d1e2cb29b71c3e8a5cdfa52e
laud <laudbentil@gmail.com>
2024-06-20 03:33:06 +0000

    update status_codes due to djoser auth, add unit tests for company create and fetch

--- SpaceFeedCore/urls.py
+++ SpaceFeedCore/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("feed.urls")),
-    path("api/v1/auth", include("djoser.urls")),
-    path("api/v1/auth", include("djoser.urls.authtoken")),
-    path("api/v1/auth", include("djoser.urls.jwt")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.authtoken")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ]
--- feed/tests/test_setup.py
+++ feed/tests/test_setup.py
@@ -1,5 +1,6 @@
-import uuid, unittest
+import unittest, uuid
 from django.contrib.auth.models import AnonymousUser
+from feed.models import Company
 from django.test import Client
 
 
@@ -12,7 +13,7 @@ class WebsiteTest(unittest.TestCase):
         Get forbidden response for unauthenticated request on websites/
         """
         response = self.client.get("/api/v1/websites/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 401)
 
 
 class FeedTest(unittest.TestCase):
@@ -24,7 +25,7 @@ class FeedTest(unittest.TestCase):
         Get forbidden response for unauthenticated request on feeds/
         """
         response = self.client.get("/api/v1/feeds/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 401)
 
 
 class LocationTest(unittest.TestCase):
@@ -36,24 +37,47 @@ class LocationTest(unittest.TestCase):
         Get forbidden response for unauthenticated request on locations/
         """
         response = self.client.get("/api/v1/locations/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 401)
 
 
 class CompanyTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
+        self.company_name = "Random Company"
 
-    def test_website_for_success_response(self):
+    def test_companies_endpoint_for_success_response(self):
         """
         Get success response for unauthenticated request
         """
         response = self.client.get("/api/v1/companies/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, [])
+        self.assertGreater(len(response.data), 0)
 
-    def test_website_for_not_found_response(self):
+    def test_companies_endpoint_for_not_found_response(self):
+        """
+        Get not found response for unauthenticated request
+        """
+        response = self.client.get("/api/v1/companies/200/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_companies_endpoint_for_created_success(self):
+        """
+        Create a company
+        """
+
+        company = Company.objects.create(
+            signature=uuid.uuid4(),
+            logo="",
+            name=self.company_name,
+            details="Random Company since 1987",
+            is_active=False,
+        )
+        self.assertEqual(company.name, self.company_name)
+
+    def test_companies_endpoint_for_found_response(self):
         """
         Get not found response for unauthenticated request
         """
         response = self.client.get("/api/v1/companies/1/")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["name"], self.company_name)
