12970fa14024730fd2f7d3766a2e428ed2e9039d
laud <laudbentil@gmail.com>
2024-06-20 00:27:31 +0000

    update tests on unauthenticated endpoints to cover Location & Feed

--- feed/tests/test_setup.py
+++ feed/tests/test_setup.py
@@ -1,9 +1,6 @@
 import uuid, unittest
 from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory, TestCase, Client
-from feed.models import User, Website
-
-from feed.views import WebsiteViewSet
+from django.test import Client
 
 
 class WebsiteTest(unittest.TestCase):
@@ -12,9 +9,33 @@ class WebsiteTest(unittest.TestCase):
 
     def test_websites_endpoint_for_forbidden_response(self):
         """
-        Get forbidden response for unauthenticated request
+        Get forbidden response for unauthenticated request on websites/
         """
         response = self.client.get("/api/v1/websites/")
+        self.assertEqual(response.status_code, 403)
+
+
+class FeedTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_feeds_endpoint_for_forbidden_response(self):
+        """
+        Get forbidden response for unauthenticated request on feeds/
+        """
+        response = self.client.get("/api/v1/feeds/")
+        self.assertEqual(response.status_code, 403)
+
+
+class LocationTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_websites_endpoint_for_forbidden_response(self):
+        """
+        Get forbidden response for unauthenticated request on locations/
+        """
+        response = self.client.get("/api/v1/locations/")
         self.assertEqual(response.status_code, 403)
 
 
