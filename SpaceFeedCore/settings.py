c83910566e5c1a1b724a3f3fce65f69e6053291d
laud <laudbentil@gmail.com>
2024-06-20 02:53:44 +0000

    add further configuration for djoser

--- SpaceFeedCore/settings.py
+++ SpaceFeedCore/settings.py
@@ -145,3 +145,11 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
 }
+
+DJOSER = {
+    "PASSWORD_RESET_CONFIRM_URL": "#/password/reset/confirm/{uid}/{token}",
+    "USERNAME_RESET_CONFIRM_URL": "#/username/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "#/activate/{uid}/{token}",
+    "SEND_ACTIVATION_EMAIL": False,
+    "SERIALIZERS": {},
+}
