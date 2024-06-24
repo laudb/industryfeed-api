2d2f499dc2eb2c164fb3221a137a7d5b5574c39c
laud <laudbentil@gmail.com>
2024-06-24 10:56:39 +0000

    create settings folder and create environment specific settings files

---
+++ SpaceFeedCore/settings/test.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    "test": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / f"test_{os.getenv('DB_NAME')}",
+    },
+}
