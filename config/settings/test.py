118fe85cbf63b5badb7eda02d9da730c40a8e7a0
laud <laudbentil@gmail.com>
2024-06-25 23:28:35 +0000

    update default database for test config

--- config/settings/test.py
+++ config/settings/test.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "test": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / f"test_{os.getenv('DB_NAME')}",
     },
