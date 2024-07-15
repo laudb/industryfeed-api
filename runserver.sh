7cc1be8e505245d40beb908b024bcef0574b40c5
laud <laudbentil@gmail.com>
2024-07-15 15:26:21 +0000

    conditionally run application depending on arguments

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python3 manage.py runserver
+
+if [ "$1" == "production" ]
+then
+    python3 manage.py runserver --settings=config.settings.production
+elif [ "$1" == "test" ]
+then
+    python3 manage.py test feed/tests/ --settings=config.settings.test
+else
+    python3 manage.py runserver --settings=config.settings.local
+fi
