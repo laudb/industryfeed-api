80b74e04645a458cf5c141882d730172aa6214cf
laud <laudbentil@gmail.com>
2024-07-23 02:50:57 +0000

    get ALLOWED_HOST, CORS_ALLOWED_ORGIN from .env file instead of hardcoded value

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,9 +3,18 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://industryfeed.netlify.app/"]
+ALLOWED_HOSTS = []
 
-CORS_ALLOWED_ORIGINS = ["https://industryfeed.netlify.app/"]
+ALLOWED_HOST = os.environ.get("ALLOWED_HOST")
+if ALLOWED_HOST:
+    ALLOWED_HOSTS.append(ALLOWED_HOST)
+
+CORS_ALLOWED_ORIGINS = []
+
+CORS_ALLOWED_ORIGIN = os.environ.get("CORS_ALLOWED_ORIGIN")
+if CORS_ALLOWED_ORIGIN:
+    CORS_ALLOWED_ORIGINS.append(CORS_ALLOWED_ORIGIN)
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
