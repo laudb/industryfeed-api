3c827951353bc105668b1d32b997acb5c8b4e677
laud <laudbentil@gmail.com>
2024-07-23 01:58:34 +0000

    add config for allowed hosts and cors allowed hosts to prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,14 +3,16 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://industryfeed.netlify.app/"]
 
+CORS_ALLOWED_ORIGINS = ["https://industryfeed.netlify.app/"]
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / os.getenv("DB_NAME"),
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DB_CONNECTION_STRING"),
+        conn_max_age=int(os.getenv("DB_CONNECTION_MAX_AGE")),
+        conn_health_checks=os.getenv("DB_CONNECTION_HEALTCH_CHECKS"),
+    )
 }
