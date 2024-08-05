4d779ed145bf684f52bb74e8248a67b92e5ad45c
laud <laudbentil@gmail.com>
2024-08-05 22:03:18 +0000

    simplify dockerfile

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [ "$1" == "production" ]
-then
-    python3 manage.py runserver --settings=config.settings.production
-elif [ "$1" == "test" ]
-then
-    python3 manage.py test feed/tests/ --settings=config.settings.test
-else
-    python3 manage.py runserver --settings=config.settings.local
-fi
+python3 manage.py runserver 0.0.0.0:8000
